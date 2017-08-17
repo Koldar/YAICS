f4212b152c0f87c1c744879f00ac819752f96ebd
Massimo Bono <mbono@unibs.it>
2017-08-17 11:09:53 +0200

    INIT
    - added basics scripts

---
+++ simple.rpc.server/commons.py
@@ -0,0 +1,50 @@
+import socketserver
+import abc
+import struct
+
+class Packet:
+
+        PACKET_QUESTION = 0
+        PACKET_ANSWER_OK = 1
+        PACKET_ANSWER_KO = 2
+
+        def __init__(self, magic_number, type, length, payload):
+                self.magic_number = magic_number
+                self.payload = payload
+                self.type = type
+                if length != len(payload):
+                        raise ValueError("can't create a malformed packet")
+
+        def __len__(self):
+                return len(self.payload)
+
+        @abc.classmethod
+        def create_answer(clazz, success):
+                type = PACKET_ANSWER_OK if success else PACKET_ANSWER_KO
+                Packet retVal = Packet(b'beefly', type, 0, b"")
+                return retVal
+
+        def __bytes__(self):
+                length = struct.pack("<L", len(self.payload))
+                type = struct.pack("c", self.type))
+                return b"".join([self.magic_number, type, length, self.payload])
+
+	@abc.classmethod
+        def parse(clazz, bytes_received):
+                #magic number
+                magic_number = bytes[0:6]
+                if magic_number != b'beefly':
+                        raise TypeError("this is not a packet we can handle")
+
+                #type
+                type = struct.unpack("c", bytes[6])[0]
+                if type not in [PACKET_QUESTION, PACKET_ANSWER]:
+                        raise TypeError("Unrecognized type")
+
+                #length
+                length = struct.unpack("<L", bytes[7:11])[0]
+
+                #payload
+                payload = bytes[11:length]
+
+                return Packet(magic_number, type, length, payload)
---
+++ simple.rpc.server/simple.rpc.server.py
@@ -0,0 +1,59 @@
+from socketserver import ThreadingMixIn
+from socketserver import TCPServer
+from socketserver import BaseRequestHandler
+from subprocess import call
+import struct
+import abc
+from commons import Packet
+
+class SimpleRPCServer(ThreadingMixIn, TCPServer):
+	pass
+
+class CommandHandler(BaseRequestHandler):
+	
+	def execute_payload(self, packet):
+		cmd = str(packet.payload)
+		cmds = cmd.split(" ")
+
+		try:
+			check_call(cmds)
+		except CallProcessError as e:
+			return e.returncode
+		return 0
+
+	def fetch_packet(self):
+		total_bytes = b""
+		packet = None
+		socket.settimeout()
+		while True:
+			bytes_received = self.request.recv(1024)
+			if bytes_received is None:
+				success = False
+				break
+			if len(bytes_received) == 0:
+				success = False
+				break
+
+			total_bytes = b"".join([total_bytes, bytes_received])
+			try:
+				packet = Packet.parse(total_bytes)
+			except Exception:
+				continue
+			else:
+			success = True
+			break
+
+		return (packet, success)
+
+	def handle(self):
+		try:
+			packet, success = self.fetch_packet()
+			if success:
+				self.execute_payload(packet)
+				reply = Packet.create_answer(True)
+			else:
+				reply = Packet.create_answer(False)
+		except TypeError:
+			reply = Packet.create_answer(False)
+		finally:
+			self.request.sendall(bytes(reply))
